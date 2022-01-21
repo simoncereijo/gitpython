316f3295d87f68d72abf348c8edd27b27e1f4e47
simon <simon.cereijo@gmail.com>
2022-01-21 19:03:44 +0100

    Version 0.3

--- aplicacion.py
+++ aplicacion.py
@@ -41,57 +41,67 @@ class Aplicacion(Tk):
             self.repo.checkout_commit(self.commits[elemento_selecciondo])
         except:
             pass
+    def validar_commit(self,comentario):
+
+        try:
+            self.repo.commit_add(str(comentario))
+            self.lista_comits(self.frame_rigth)
+            print(self.repo.show_commits())
+        except:
+            pass
+
+
 
     def frame_izquierdo(self):#botones/textos dentro del frame del lado izquierdo
 
         #declaramos Frames
-        frame_left=Frame(self)
-        frame_left.config(width=1200/2, height=710,relief="sunken",bd=1)
-        frame_left.grid(row=0,column=0)
-        frame_left.grid_propagate(0)
+        self.frame_left=Frame(self)
+        self.frame_left.config(width=1200/2, height=710,relief="sunken",bd=1)
+        self.frame_left.grid(row=0,column=0)
+        self.frame_left.grid_propagate(0)
 
         #Encabezado
         cabecera="CONFIGURACION DEL REPOSITORIO"
-        encabezado=Label(frame_left,text=cabecera,height=3,font=("Arial",15))
+        encabezado=Label(self.frame_left,text=cabecera,height=3,font=("Arial",15))
         f = font.Font(encabezado, encabezado.cget("font"))
         f.configure(underline=True)
         encabezado.configure(font=f)
         encabezado.grid(row=0,column=0,columnspan=2)
         #Usuario
-        label_user=Label(frame_left,text="User:",width=10)
+        label_user=Label(self.frame_left,text="User:",width=10)
         label_user.grid(row=1,column=0)
-        entry_user=Entry(frame_left,width=40)
+        entry_user=Entry(self.frame_left,width=40)
         entry_user.grid(row=1,column=1)
         #Email
-        label_email=Label(frame_left,text="Email:",width=10)
+        label_email=Label(self.frame_left,text="Email:",width=10)
         label_email.grid(row=2,column=0)
-        entry_email=Entry(frame_left,width=40)
+        entry_email=Entry(self.frame_left,width=40)
         entry_email.grid(row=2,column=1)
         #ruta de la carpeta donde crear el repositorio
-        boton_busqueda_cd=Button(frame_left,text="Archivo...",width=10,command=lambda :self.crear_label_directorio(frame_left,entry_user.get(),entry_email.get()))
+        boton_busqueda_cd=Button(self.frame_left,text="Archivo...",width=10,command=lambda :self.crear_label_directorio(self.frame_left,entry_user.get(),entry_email.get()))
         boton_busqueda_cd.grid(row=3,column=0)
         #separador
-        separador=Label(frame_left,text="------------------------------------------------------------------------------------------------------------")
+        separador=Label(self.frame_left,text="------------------------------------------------------------------------------------------------------------")
         separador.grid(row=5,column=0,columnspan=20)
         #comit repo
-        entrada_commit=Entry(frame_left,width=48)
+        entrada_commit=Entry(self.frame_left,width=48)
         entrada_commit.grid(row=6,column=0)
-        boton_commit=Button(frame_left,text="Validar nuevo Commit",width=40,command=lambda :self.repo.commit_add(entrada_commit.get()))
+        boton_commit=Button(self.frame_left,text="Validar nuevo Commit",width=40,command=lambda :self.validar_commit(entrada_commit.get()))
         boton_commit.grid(row=7,column=0)
         #separador
-        separador=Label(frame_left,text="------------------------------------------------------------------------------------------------------------")
+        separador=Label(self.frame_left,text="------------------------------------------------------------------------------------------------------------")
         separador.grid(row=8,column=0,columnspan=20)
         ##checkout repo
 
-        boton_commit_checkout=Button(frame_left,text="Recuperar version",width=40,command=lambda :self.checkout_versio())
+        boton_commit_checkout=Button(self.frame_left,text="Recuperar version",width=40,command=lambda :self.checkout_versio())
         boton_commit_checkout.grid(row=10,column=0)
         #separador
-        separador=Label(frame_left,text="------------------------------------------------------------------------------------------------------------")
+        separador=Label(self.frame_left,text="------------------------------------------------------------------------------------------------------------")
         separador.grid(row=11,column=0,columnspan=20)
         #push a github
-        entrada_repo_github=Entry(frame_left,width=48)
+        entrada_repo_github=Entry(self.frame_left,width=48)
         entrada_repo_github.grid(row=12,column=0)
-        boton_push=Button(frame_left,text="Push a Github",width=40,command=lambda :self.repo.push_github(entrada_repo_github.get()))
+        boton_push=Button(self.frame_left,text="Push a Github",width=40,command=lambda :self.repo.push_github(entrada_repo_github.get()))
         boton_push.grid(row=13,column=0)
 
 
@@ -123,15 +133,15 @@ class Aplicacion(Tk):
     def frame_derecho(self):#Botones/textos dentro del frame del lado derecho
 
 
-        frame_rigth=Frame(self)
-        frame_rigth.config(width=1200 / 2, height=710, relief="sunken", bd=1)
-        frame_rigth.grid(row=0,column=1)
-        frame_rigth.grid_propagate(0)
+        self.frame_rigth=Frame(self)
+        self.frame_rigth.config(width=1200 / 2, height=710, relief="sunken", bd=1)
+        self.frame_rigth.grid(row=0,column=1)
+        self.frame_rigth.grid_propagate(0)
 
         #Creo botones
 
 
-        boton_reflog=Button(frame_rigth,text="Reflog",width=90,command=lambda :self.lista_comits(frame_rigth))
+        boton_reflog=Button(self.frame_rigth,text="Reflog",width=90,command=lambda :self.lista_comits(self.frame_rigth))
         boton_reflog.grid(row=0, column=0)
 
 
